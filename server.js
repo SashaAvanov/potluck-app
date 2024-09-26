f50ff12372012faaba1e0cd7c3cdb2cfe57750b2
Sasha Avanov <sasha.devanov@gmail.com>
2024-09-26 09:34:43 -0700

    test commit

--- server.js
+++ server.js
@@ -1,12 +1,51 @@
 const express = require('express')
 const app = express()
-const PORT = 8000
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session') //requires sessions 
+const MongoStore = require('connect-mongo')//handles sessions and cookies
+const methodOverride = require('method-override')
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const eventRoutes = require('./routes/event')
+
+require('dotenv').config({path: './config/.env'})
+
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(logger('dev'))
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+//use forms for put/delete
+app.use(methodOverride("_method"))
+
+// Sessions
+app.use(session({
+  secret: 'potluck',
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.DB_STRING
+  })
+}));
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+
+app.use('/', mainRoutes)
+app.use('/event', eventRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running')
+})    
